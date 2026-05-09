5f622c46db994ee114e90944f06721b48af12629
Cesa-V <vitorcesa0@gmail.com>
2026-05-09 10:54:18 -0300

    Remove gid parameter from GOOGLE_SHEET_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   APP_NAME: 'AJ Finance',
   GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbze00WSCPqs5BjyJ-y3644OBuWJaaHRZPe9RAuFO0sec7L91S_wbfBdXB3E4cwogYbJ/exec',
-  GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1MgT7aZI9deFc30dlmCgzH9Gmbq9cA7-4XtZoDPEHl0A/edit?gid=722876405#gid=722876405',
+  GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1MgT7aZI9deFc30dlmCgzH9Gmbq9cA7-4XtZoDPEHl0A/edit',
   DEMO_MODE: false
 };
